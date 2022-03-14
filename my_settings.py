0c929e2c19a20e28bf776c4108a4fd968a5f2bef
sangahnhan <sangahn0524@gmail.com>
2022-03-14 16:19:06 +0900

    Add: 초기셋팅 설정 완료

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES={
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'ownerdog',
+        'USER' : 'root',
+        'PASSWORD' : '23571113',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-kmxiae201h1t%v-c1$!evm+at!30e^7f$pnujh=69906evchh7'
